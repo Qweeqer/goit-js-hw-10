c35673c08087a2a930b0507f418fa3539a702c73
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-07-13 13:27:17 +0300

    import notiflix lodash

---
+++ src/fetchCountries.js
@@ -0,0 +1,3 @@
+export const fetchCountries=countryName=> {
+    return fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`)  
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputBox = document.querySelector('#search-box');
+console.log(inputBox);
+const countryList = document.querySelector('.country-list');
+console.log(countryList);
